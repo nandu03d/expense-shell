d8548934d50ea8a15a940dd9b3e87906a6b4928f
nandu03d <nandu03d@gmail.com>
2024-06-26 17:36:29 +0530

    commit-message

---
+++ backend.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+userid=$(id -u)
+timeStamp=$(date +%F-%H-%M-%S)
+scriptName=$(echo $0 | cut -d "." -f1)
+logFile=/tmp/$scriptName-$timeStamp.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $userid -ne 0 ]
+then
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+dnf module disable nodejs -y &>>$logFile
+validate $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$logFile
+validate $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>>$logFile
+validate $? "Installing nodejs
+
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
