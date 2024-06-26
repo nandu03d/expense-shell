fdacbd8e3baef4efc8f4bd14bbb5d06feef72809
nandu03d <nandu03d@gmail.com>
2024-06-26 11:30:01 +0530

    commit-message

---
+++ mysql.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+userid=$(id -u)
+timeStamp=$(date +%F-%H-%M-%S)
+scriptName=$(echo $0 | cut -d "." -f1)
+logFile=/tmp/$scriptName-$timeStamp.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $userid -ne 0 ]
+then
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up root password"
